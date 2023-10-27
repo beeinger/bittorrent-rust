957fe924ffb4638c1a2a3ffd0aa7700d80f20d73
beeinger <sulecki.maciek@gmail.com>
2023-10-27 15:34:04 +0100

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,44 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    let next = encoded_value.chars().next().unwrap();
+    if next == 'l' {
+        let list_string = &encoded_value[1..encoded_value.len() - 1];
+        let mut list = Vec::new();
+
+        let mut remaining = list_string;
+        while !remaining.is_empty() {
+            let (decoded_value, new_remaining) = decode_bencoded_value(remaining);
+            list.push(decoded_value);
+            remaining = new_remaining;
+        }
+
+        (
+            serde_json::Value::Array(list),
+            &encoded_value[encoded_value.len()..],
+        )
+    } else if next == 'i' {
+        let e_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..e_index];
+        let number = number_string.parse::<i64>().unwrap();
+
+        (
+            serde_json::Value::Number(number.into()),
+            &encoded_value[e_index + 1..],
+        )
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    } else if next.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-        let number_string = &encoded_value[1..encoded_value.len() - 1];
-        let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(number.into());
+
+        (
+            serde_json::Value::String(string.to_string()),
+            &encoded_value[colon_index + 1 + number as usize..],
+        )
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -31,7 +56,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", decoded_value.0.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
